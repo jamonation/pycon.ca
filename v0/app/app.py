2d162149f1c684eafd5bb1df2e6c541877a150ca
Jamon Camisso <jamon@digitalocean.com>
2019-11-16 16:11:09 -0500

    Update v0 app to show hostname in Hello World message

--- v0/app/app.py
+++ v0/app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from socket import gethostname
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! From {}'.format(gethostname())
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
